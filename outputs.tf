8691518c1fbd1de4658ee1f03eeb1dc42a39cf77
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2019-03-08 15:16:35 -0800

    output: remove nil output from terraform module

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,15 @@
 output "cidr_block" {
   description = "Output the cidr_block used within this network"
-  value       = "${aws_vpc.dcos_vpc.cidr_block}"
+  value       = "${coalesce(aws_vpc.dcos_vpc.cidr_block, "")}"
+
+  # TODO(mbernadin): remove coalesce when nil on destroy is fixed: https://github.com/hashicorp/terraform/issues/17862
 }
 
 output "aws_main_route_table_id" {
   description = "AWS main route table id"
-  value       = "${aws_vpc.dcos_vpc.main_route_table_id}"
+  value       = "${coalesce(aws_vpc.dcos_vpc.main_route_table_id, "")}"
+
+  # TODO(mbernadin): remove coalesce when nil on destroy is fixed: https://github.com/hashicorp/terraform/issues/17862
 }
 
 output "subnet_ids" {
@@ -20,5 +24,7 @@ output "subnets" {
 
 output "vpc_id" {
   description = "AWS VPC ID"
-  value       = "${aws_vpc.dcos_vpc.id}"
+  value       = "${coalesce(aws_vpc.dcos_vpc.id, "")}"
+
+  # TODO(mbernadin): remove coalesce when nil on destroy is fixed: https://github.com/hashicorp/terraform/issues/17862
 }
